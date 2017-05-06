bfa385b11ded1ebd8ed20a43b1d69258fc646b43
AbdelrahmanRadwan <abdelrahman_hamdy_radwan@yahoo.com>
2017-05-06 06:14:41 +0200

    Finish the MLP training

--- WindowsFormsApplication1/Math Operations/MathematicalOperations.cs
+++ WindowsFormsApplication1/Math Operations/MathematicalOperations.cs
@@ -7,6 +7,65 @@ namespace WindowsFormsApplication1
 {
     public static class MathematicalOperations
     {
+        static private Random random = new Random();
+        static public double[] DifferentiationOfSoftmaxFunction(double []x)
+        {
+            double[] derivative= new double[x.Length];
+            for(int i=0;i<x.Length;i++)
+                derivative [i]= (1 - x[i]) * x[i];
+            return derivative;
+        }
+        public static double GetRandomNumber(double minimum, double maximum)
+        {           
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+        public static double[] softmax(double[] oSums)
+        {
+            // determine max output sum
+            // does all output nodes at once so scale doesn't have to be re-computed each time
+            double max = oSums[0];
+            for (int i = 0; i < oSums.Length; ++i)
+                max = Math.Max(max, oSums[i]);
+
+            // determine scaling factor -- sum of exp(each val - max)
+            double scale = 0.0;
+            for (int i = 0; i < oSums.Length; ++i)
+                scale += Math.Exp(oSums[i] - max);
+
+            double[] result = new double[oSums.Length];
+            for (int i = 0; i < oSums.Length; ++i)
+                result[i] = Math.Exp(oSums[i] - max) / scale;
+            return result;
+        }
+        public static double CalculateErrorUsingSoftmax(double[] oSums, double []DesiredOutput)
+        {
+            // determine max output sum
+            // does all output nodes at once so scale doesn't have to be re-computed each time
+            double max = oSums[0];
+            for (int i = 0; i < oSums.Length; ++i)
+                max = Math.Max(max,oSums[i]);
+
+            // determine scaling factor -- sum of exp(each val - max)
+            double scale = 0.0;
+            for (int i = 0; i < oSums.Length; ++i)
+                scale += Math.Exp(oSums[i] - max);
+
+            double[] result = new double[oSums.Length];
+            for (int i = 0; i < oSums.Length; ++i)
+                result[i] = Math.Exp(oSums[i] - max) / scale;
+            //Result now scaled so that xi sum to 1.0
+            //////////////////////////////////////////////////////////////////////////////
+            ////
+            //calculate the error (e) = sum((desired - softmax result)^2)/2
+            ////
+            double sum = 0;
+            for (int i = 0; i < oSums.Length; i++)
+            {
+                DesiredOutput[i] = (DesiredOutput[i] - result[i]) * (DesiredOutput[i] - result[i]);
+                sum += DesiredOutput[i];
+            }
+                return sum/2.0; 
+        }
         ////
         // The tanh function
         ////
@@ -15,6 +74,17 @@ namespace WindowsFormsApplication1
             double y = Math.Tanh(X);
             return y;
         }
+        public static double DifferentiationOfHyperbolicFunction(double x)
+        {
+            double y;
+            y = 4 * Math.Exp(2 * x);
+            y = y / ((1 + Math.Exp(2 * x)) * (1 + Math.Exp(2 * x)));
+            return y;
+        }
+
+            /////
+            // Matrix Operations
+            /////
         private static double[,] MatrixTranspose(int n, int m, double[,] Matrix)
         {
             double[,] TransposedMatrix = new double[m, n];
@@ -41,6 +111,7 @@ namespace WindowsFormsApplication1
                     Sum = Sum + NewWeights[i, j] * NewX[j];
                 result[i] = Sum;
             }
+
             return result;
         }
 
--- WindowsFormsApplication1/The Model Package/Training/MultilayerPerceptronModel.cs
+++ WindowsFormsApplication1/The Model Package/Training/MultilayerPerceptronModel.cs
@@ -19,12 +19,18 @@ namespace WindowsFormsApplication1
         double TrainingMeanSquaredError;  // MSE for training data, 1 is just an initalization value.
         double Error; // a counter to denote the number of error outputs
         double[,] Eta;
+        double SumOfErrorPerEpoch;
+        double EtaSingleValue;
         double[] MeanSquaredError;
         double[,] TrainingFeatures;
+        double[] TrainingError;
         string[] TrainingLabels;
         Dictionary<int, double[,]> Weights; // mapping the weights layer, to its real weights.
-        Dictionary<string, int> LabelsEncoding; // to transform the labels from strings to integers. // 0 based
+        Dictionary<int, double[]> X; // The feature vector
+        Dictionary<int, double[]> NeoronsSignalErrors; // The Signal Error vector
+        Dictionary<string, double[]> DesiredOutput; // to transform the labels from strings to integers. // 0 based
         string[] Labels = { "Closing Eyes", "Looking Down", "Looking Front", "Looking Left" }; // 0 based
+
         public MultilayerPerceptronModel()
         {
             NumberOfInputNeurons = 19;
@@ -32,6 +38,7 @@ namespace WindowsFormsApplication1
             NumberOfOutputNeorons = 4;
             NumberOfTrainingSamples = 60;
             Weights = new Dictionary<int, double[,]>();
+            TrainingError = new double[NumberOfTrainingSamples];
         }
         ////
         // The entry point of the Algorithms (the run of it), which is called from the main form.
@@ -69,6 +76,14 @@ namespace WindowsFormsApplication1
             Weights[NumberOfHiddenLayers] = new double[NumberOfNeuronsInEachHiddenLayer[NumberOfHiddenLayers - 1], NumberOfOutputNeorons];
             ////////////////////////////////////////////////////////////////////////////
             ////
+            // Initialize the Weigts by valuse between -1 and 1
+            ////
+            for (int i = 0; i < NumberOfHiddenLayers + 1; i++)
+                for (int j = 0; j < Weights[i].GetLength(0); j++)
+                    for (int k = 0; k < Weights[i].GetLength(1); k++)
+                        Weights[i][j, k] = MathematicalOperations.GetRandomNumber(-1, 1);
+            ////////////////////////////////////////////////////////////////////////////
+            ////
             // Initialize the Eta 
             ////
             double[] temp = Annealing(0.1, 1E-5, NumberOfEpochs);
@@ -78,10 +93,10 @@ namespace WindowsFormsApplication1
             ////
             //Initialize the Labels Encoding
             ////
-            LabelsEncoding["Closing Eyes"] = 0;
-            LabelsEncoding["Looking Down"] = 1;
-            LabelsEncoding["Looking Front"] = 2;
-            LabelsEncoding["Looking Left"] = 3;
+            DesiredOutput["Closing Eyes"] = new double[] { 1.0, 0.0, 0.0, 0.0 };
+            DesiredOutput["Looking Down"] = new double[] { 0.0, 1.0, 0.0, 0.0 };
+            DesiredOutput["Looking Front"] = new double[] { 0.0, 0.0, 1.0, 0.0 };
+            DesiredOutput["Looking Left"] = new double[] { 0.0, 0.0, 0.0, 1.0 };
             ////
             //Load the training data set and labels
             ////
@@ -96,21 +111,89 @@ namespace WindowsFormsApplication1
         {
             while (TrainingMeanSquaredError > MeanSquaredErrorThreshold && CurrentEpoch <= NumberOfEpochs)
             {
-                for(int i=0;i<NumberOfTrainingSamples;i++)
+                ////
+                // Forward Computation
+                ////     
+                for (int i = 0; i < NumberOfTrainingSamples; i++)
                 {
-                    ////
-                    // Forward Computation
-                    ////
-                    
-                    for(int j=0;j<NumberOfHiddenLayers+1;j++)
+                    X[0] = RowSlicer(i);
+                    for (int j = 0; j < NumberOfHiddenLayers + 1; j++)
                     {
-                        double[] X;
-                        if(j==0)
-                            X = new double(RowSlicer(i));
-                        X = MathematicalOperations.MatrixMatrixMultiplycation(Weights[j].GetLength(0), Weights[j].GetLength(1), X.Length, X, Weights[j]);
+
+                        X[j + 1] = MathematicalOperations.MatrixMatrixMultiplycation(
+                            Weights[j].GetLength(0), // weight n
+                            Weights[j].GetLength(1), // weight m
+                            X[j].Length, // vector of features' size
+                            X[j], // vector of featurtes
+                            Weights[j]);
+                        if (j != NumberOfHiddenLayers)
+                        {
+                            ////
+                            //Run the Hyperbolic Function over the array data
+                            ////
+                            for (int k = 0; k < X[j + 1].Length; k++)
+                                X[j + 1][k] = MathematicalOperations.HyperbolicFunction(X[j + 1][k]);
+                        }
+                        else
+                        {
+                            ////
+                            //Run the softmax Function over the array data
+                            ////
+                            X[j + 1] = MathematicalOperations.softmax(X[j + 1]);
+                        }
                     }
+                    //Calculate the error of the feedforward part.
+                    TrainingError[i] = MathematicalOperations.CalculateErrorUsingSoftmax(
+                                                                                        X[NumberOfHiddenLayers],
+                                                                                        DesiredOutput[TrainingLabels[i]]
+                                                                                        );
+
+                    ////
+                    //  Backward computation
+                    //// 
+                    ///////////////////////////////////////
+                    ////
+                    // Calculate the Neorons Signal Errors
+                    ////
+                    /////For the output layer using softmax
+                    for (int j = 0; j < NumberOfOutputNeorons; j++)
+                        NeoronsSignalErrors[NumberOfHiddenLayers] = MathematicalOperations.DifferentiationOfSoftmaxFunction(X[NumberOfHiddenLayers]);
+                    //// for the other layers
+                    for (int j = NumberOfHiddenLayers - 1; j >= 0; j--)
+                    {
+                        NeoronsSignalErrors[j] = new double[X[j].Length];
+                        // pick one node of the hidden layer.
+                        for (int k = 0; k < X[j].Length; k++)
+                        {
+                            double SumOfErrorSignals = 0;
+                            for (int l = 0; l < NeoronsSignalErrors[j + 1].Length; l++)
+                                SumOfErrorSignals = SumOfErrorSignals + NeoronsSignalErrors[j + 1][l] * Weights[j + 1][k, l];
+                            // Update the Signal Error
+                            NeoronsSignalErrors[j][k] = SumOfErrorSignals;
+                        }
+                    }
+                    ///////////////////////////////////////////////////////////////////////////////////
+                    //////Update the Weights
+                    ///////////////////////////////////////////////////////////////////////////////////
+                    for (int j = 0; j < NumberOfHiddenLayers + 1; j++)
+                        for (int k = 0; k < Weights[i].GetLength(0); k++)
+                            for (int l = 0; l < Weights[i].GetLength(1); l++)
+                                for (int m = 0; m < NeoronsSignalErrors[j].Length; m++)
+                                    Weights[j][k, l] =
+                                        Weights[j][k, l] + // Weight
+                                        EtaSingleValue * // eta
+                                        NeoronsSignalErrors[j][k]* // Signal Error
+                                        MathematicalOperations.DifferentiationOfHyperbolicFunction(X[j + 1][k]) * // Diffrentiation of the next feature.
+                                        X[j][k]; // Feature
 
                 }
+                /////
+                // Calculate the mean squared error
+                ////
+                for(int j=0;j<NumberOfTrainingSamples;j++)
+                    SumOfErrorPerEpoch= SumOfErrorPerEpoch + TrainingError[j]*TrainingError[j];
+                SumOfErrorPerEpoch/=NumberOfTrainingSamples;
+                MeanSquaredError[CurrentEpoch] = SumOfErrorPerEpoch / 2;
             }
         }
 
